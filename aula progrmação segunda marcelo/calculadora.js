50404a0aa4303d5f7d7cc2d149b8301ff3e02bcf
Marcelo <marcelo.28117@gmail.com>
2024-06-03 12:41:16 -0300

    Add files via upload

---
+++ aula progrmação segunda marcelo/calculadora.js
@@ -0,0 +1,13 @@
+let x = 30
+let y = 20
+let a = "tudo bem"
+let b = "está devendo"
+let Calculo = x + y
+if(
+    Calculo = 100
+) {
+    console.log(a)
+} else {
+    console.log(b)
+}
+console.log(Calculo)
