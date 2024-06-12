aa6b85a03fac3edcdc53ad36d05c51b6d5806d50
Marcelo <marcelo.28117@gmail.com>
2024-06-12 12:37:38 -0300

    aula quarta 12.06

---
+++ tipo.js
@@ -0,0 +1,9 @@
+let fruta = "abacate, "
+let frutas = ["maçã", " kiwi", " uva"]
+let banana = {
+    "cor": "amarela",
+    "leguminosa": "não",
+    "fruta": "sim",
+    "sabor": "doce"
+}
+console.log("A cor da banana é: " + banana.cor, "ela " + banana.leguminosa + " é a fruta favorita de Maria, mas " + banana.fruta + " de Bia, já que bia gosta de comida " + banana.sabor)
