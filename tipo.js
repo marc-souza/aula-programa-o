04c4f3b7ef682ae0bb15a603c47251f162aef770
Marcelo <marcelo.28117@gmail.com>
2024-06-13 11:51:51 -0300

    Aula de objeto composto Quinta

--- tipo.js
+++ tipo.js
@@ -1,9 +1,30 @@
 let fruta = "abacate, "
 let frutas = ["maçã", " kiwi", " uva"]
 let banana = {
-    "cor": "amarela",
-    "leguminosa": "não",
-    "fruta": "sim",
-    "sabor": "doce"
+    "cor": "Amarela",
+    "leguminosa": "Não",
+    "fruta": "Sim",
+    "sabor": "Doce"
 }
-console.log("A cor da banana é: " + banana.cor, "ela " + banana.leguminosa + " é a fruta favorita de Maria, mas " + banana.fruta + " de Bia, já que bia gosta de comida " + banana.sabor)
+let batata = {
+    "cor": "Amarela",
+    "leguminosa": "Não",
+    "fruta": "Não",
+    "sabor": "Levemente terroso"
+}
+let colegas = {
+    "Breno": {
+        "apelido": "Jogador de lol",
+        "alto": "Não",
+        "cor do tênis": "Preto",
+        "frequenta": "Sim"
+    },
+    "Júlia":  {
+        "apelido": "Julinha",
+        "alto": "Não",
+        "cor do tênis": "Branco",
+        "frequenta": "Sim"
+    }
+}
+console.log("A cor da batata é: " + batata.cor + ". Ela é leguminosa? " + batata.leguminosa + ". Ela é uma fruta? " + batata.fruta + ". E qual é o seu sabor? " + batata.sabor)
+console.log("A cor da banana é: " + banana.cor + ". Ela é leguminosa? " + banana.leguminosa + ". Ela é uma fruta? " + banana.fruta + ". E qual é o seu sabor? " + banana.sabor)
