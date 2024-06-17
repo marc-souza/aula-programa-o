d9f8d2f1884d9aa35a3a750ecf2a2a86604d6713
Marcelo <marcelo.28117@gmail.com>
2024-06-17 12:36:01 -0300

    aula segunda junina

--- tipo.js
+++ tipo.js
@@ -28,3 +28,4 @@ let colegas = {
 }
 console.log("A cor da batata é: " + batata.cor + ". Ela é leguminosa? " + batata.leguminosa + ". Ela é uma fruta? " + batata.fruta + ". E qual é o seu sabor? " + batata.sabor)
 console.log("A cor da banana é: " + banana.cor + ". Ela é leguminosa? " + banana.leguminosa + ". Ela é uma fruta? " + banana.fruta + ". E qual é o seu sabor? " + banana.sabor)
+console.log("Quem é o seu colega?" + Object.keys)
---
+++ verf.js
@@ -0,0 +1,3 @@
+if(1 == true) console.log("1 = true")
+if(0== false) console.log("0 = false")
+0
