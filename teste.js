bacc7c90a6d2bbd3ae5d6b93e05e6d23a21058f1
Marcelo <marcelo.28117@gmail.com>
2024-07-10 12:29:49 -0300

    aula de interpretação

---
+++ teste.js
@@ -0,0 +1,8 @@
+// mateus  musico    corda violao partiu lojas locais diferentes loc = canela busc 3 lojas = av7 piedade gamboa    barata = + longe  quer ir na = + barata qual loja ele iria
+let mateus = "musico"
+let caracsMateus = {
+    "corda violao": "quebrado",
+    "loc": "canela",
+}
+let locLojas = ["av7", "gamboa", "piedade"]
+if (mateus)
