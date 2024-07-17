33f4dc8f4d65b597e00ca622f08c7b49e09e4f8a
Marcelo <marcelo.28117@gmail.com>
2024-07-17 12:19:41 -0300

    aula de interpretação

---
+++ code.js
@@ -0,0 +1,19 @@
+// mateus musico corda violao partiu lojas locais diferentes loc = canela busc 3 lojas = av7 gamboa peidade barata = + longe quer ir na = + barata qual loja ele iria
+let mateus = "musico"
+let caracsMateus = {
+    "corda violao": "quebrado",
+    "loc": "canela",
+}
+let locLojas = ["av7", "gamboa","piedade" ]
+
+// calça 4 bolso 2 primeiros + fundo ,  1 bolso = celular cartwiea chave 2 bolso chave2 cartao de visita e cadeado
+
+let bolsosF = ["bolso1", "bolso2"]
+let calça = {
+    "bolso1": [" celular "," carteira ", " chave1"],
+    "bolso2": [" chave2 ", " cVis ", " cad "],
+    "bolso3": [],
+    "bolso4": []
+} 
+console.log("Na minha calça tem 4 bolsos, no primeiro bolso contém:" + calça.bolso1, "no segundo bolso, contém:" + calça.bolso2 + "e os bolsos 3 e 4 estão vazios" , "e os bolsos fundos são " + bolsosF)
+
