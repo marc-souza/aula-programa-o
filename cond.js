cb8b3a7a613a798cee4b58aaac2eb5a585da199a
Marcelo <marcelo.28117@gmail.com>
2024-07-30 12:21:12 -0300

    aula terça condição

---
+++ cond.js
@@ -0,0 +1,24 @@
+let calculadora = 10 + 10
+let carteira = calculadora
+/* aqui é o comando "se"*/ if( 
+    /* aqui é a condição */ carteira == 20 
+) {
+    /* aqui é a afirmação 1 */console.log(true)
+} /* afirmaçao2 */ else  {
+    console.log("está devendo")
+}
+
+ if (
+    carteira < 20
+) {
+    console.log(false)    
+} else {
+    console.log("está pago")
+}
+
+/*if = se .comando
+ (condição) = uma pergunta para ver se é true or false
+ afirmação1 = o que ele vai fazer se a condição for atendida
+  [else afirmação2] = caso a afirmação não for atendida
+  
+  linha = if (condição) afirmação1 [else afirmação2]*/
