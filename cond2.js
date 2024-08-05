fbf49ffe4fddb43ab3832faea3eecb48ce327532
Marcelo <marcelo.28117@gmail.com>
2024-08-05 12:34:37 -0300

    Aula de condicional 2  e for

---
+++ cond2.js
@@ -0,0 +1,59 @@
+/*let numero = 10
+
+if(
+    numero == 20
+) {
+    console.log ("número é 20")
+} else
+  {
+    console.log("está errado, numero é " + numero )
+}*/
+/*let pessoas = {
+    "Mario" : 17,
+    "Suzana" : 18
+}
+let idadePer = 18
+
+if(
+    Object.pessoas == 18
+) {
+    console.log("pode entrar")
+} else {
+    console.log("não entra")
+}/*
+
+/*var i = 0;
+
+for ([inicialização]; [condição]; [expressão final]) declaração*/
+
+/*let idadePessoas = {
+    "Mario": 17,
+    "Alana": 18,
+    "João": 18,
+    "Júlia":17
+ }
+
+ let i = 0;
+let idadeArray = [17, 18, 17, 15, 12]
+
+for (; idadeArray[i] > 16; i++) {
+  console.log(idadeArray[i]);
+  // more statements
+}*/
+
+let idadePessoas = {
+    "Mario": 17,
+    "Alana": 18,
+    "João": 18,
+    "Júlia":17,
+
+ }
+
+ let i = 0;
+let idadeArray = [17, 18, 17, 15, 12]
+
+for (; idadePessoas[i] > 16; i++) {
+  console.log(idadePessoas[i]);
+  // more statements
+}
+
